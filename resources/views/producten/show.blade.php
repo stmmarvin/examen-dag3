3daa7ffbc122bb1375deac18bf251a9637d5499d
EmiledeJong338569 <150025530+EmiledeJong338569@users.noreply.github.com>
2026-07-03 13:22:27 +0200

    Detail page: finale verbeteringen met betere padding en achtergrondkleur

--- resources/views/producten/show.blade.php
+++ resources/views/producten/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="py-8 bg-gray-100 min-h-screen">
-        <div style="max-width: 800px; margin-left: 2rem; padding-left: 1.5rem; padding-right: 1.5rem;">
+    <div class="py-10 bg-gray-50 min-h-screen">
+        <div style="max-width: 850px; margin-left: 3rem; padding-left: 2rem; padding-right: 2rem;">
             
             <!-- Breadcrumb -->
             <div class="mb-8">
