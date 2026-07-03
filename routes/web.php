0455cbcf65563888aa6a5e63dda6cb10a28b8813
Marvin <marvinakpabot@gmail.com>
2026-07-03 12:58:30 +0200

    Register klanten routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\KlantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,15 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/klanten', [KlantController::class, 'index'])->name('klanten.index');
+    Route::get('/klanten/details', [KlantController::class, 'details'])->name('klanten.details');
+    Route::get('/klanten/details/wijzigen', [KlantController::class, 'detailsEdit'])->name('klanten.details.edit');
+    Route::redirect('/klanten/5', '/klanten/details');
+    Route::redirect('/klanten/5/wijzigen', '/klanten/details/wijzigen');
+    Route::get('/klanten/{id}', [KlantController::class, 'show'])->name('klanten.show');
+    Route::get('/klanten/{id}/wijzigen', [KlantController::class, 'edit'])->name('klanten.edit');
+    Route::patch('/klanten/{id}', [KlantController::class, 'update'])->name('klanten.update');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
