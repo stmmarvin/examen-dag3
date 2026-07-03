83b7e2df15b6402ee2002b89d055ffb5fc884335
EmiledeJong338569 <150025530+EmiledeJong338569@users.noreply.github.com>
2026-07-03 13:21:17 +0200

    Detail page: verbeterde success message met icon en betere styling

--- resources/views/producten/show.blade.php
+++ resources/views/producten/show.blade.php
@@ -19,11 +19,12 @@
 
             <!-- Success Message -->
             @if(session('success'))
-                <div id="success-message" class="bg-green-50 border-l-4 border-green-500 p-4 mb-6">
-                    <div class="flex">
-                        <div class="ml-3">
-                            <p class="text-sm font-medium text-green-800">{{ session('success') }}</p>
-                        </div>
+                <div id="success-message" class="bg-green-100 border-l-4 border-green-600 p-4 mb-6 rounded-r shadow-sm">
+                    <div class="flex items-center">
+                        <svg class="h-5 w-5 text-green-600 mr-3" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                        </svg>
+                        <p class="text-sm font-semibold text-green-800">{{ session('success') }}</p>
                     </div>
                 </div>
                 <script>
