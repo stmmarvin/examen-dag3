118925a8ab3c38f2b7c330eab39fb9866a91330d
RensLisman <337895@student.mboutrecht.nl>
2026-07-07 09:25:50 +0200

    Commit ffix marvin

--- database/migrations/2026_07_03_124756_create_producten_table.php
+++ database/migrations/2026_07_03_124756_create_producten_table.php
@@ -8,18 +8,20 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('producten', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('categorie_id')->nullable();
-            $table->string('naam');
-            $table->text('omschrijving')->nullable();
-            $table->string('merk')->nullable();
-            $table->string('ean_code')->nullable();
-            $table->date('houdbaarheidsdatum')->nullable();
-            $table->decimal('inkoop_prijs', 10, 2)->nullable();
-            $table->decimal('verkoop_prijs', 10, 2)->nullable();
-            $table->timestamps();
-        });
+        if (! Schema::hasTable('producten')) {
+            Schema::create('producten', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('categorie_id')->nullable();
+                $table->string('naam');
+                $table->text('omschrijving')->nullable();
+                $table->string('merk')->nullable();
+                $table->string('ean_code')->nullable();
+                $table->date('houdbaarheidsdatum')->nullable();
+                $table->decimal('inkoop_prijs', 10, 2)->nullable();
+                $table->decimal('verkoop_prijs', 10, 2)->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     public function down(): void
